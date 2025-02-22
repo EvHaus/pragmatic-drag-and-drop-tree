d956a355a4177541196247be7e5cb46f78c08023
Ev Haus <ev@haus.gg>
2025-02-22 15:15:20 -0800

    Blocked by https://github.com/oven-sh/bun/issues/20902

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-// @ts-expect-error Something broke in the latest version. Hopefully can remove this once it's fixed.
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
-
-export default defineConfig({
-	plugins: [react(), tsconfigPaths()],
-	root: 'examples',
-});
