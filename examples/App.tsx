0e6a16aea5c6ba4a3cfb51fe62e35fa02c1174d9
Ev Haus <ev@haus.gg>
2025-03-10 22:51:49 -0700

    docs: Remove unused styles in example

--- examples/App.tsx
+++ examples/App.tsx
@@ -6,15 +6,15 @@ import WithGhostIndicator from './variants/with-ghost-indicator';
 const App = () => {
 	return (
 		<div className={styles.main}>
-			<div className={styles.variant}>
+			<div>
 				<h2>With Container Scroll</h2>
 				<WithContainerScroll />
 			</div>
-			<div className={styles.variant}>
+			<div>
 				<h2>With Drag Handles</h2>
 				<WithDragHandles />
 			</div>
-			<div className={styles.variant}>
+			<div>
 				<h2>With Ghost Indicator</h2>
 				<WithGhostIndicator />
 			</div>
